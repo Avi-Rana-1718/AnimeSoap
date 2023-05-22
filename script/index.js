aed5b99a944384e82e37858596e76d20536c1cb0
Avi Rana <itzavi1718@gmail.com>
2023-05-22 20:20:32 +0530

    Better error handling & added popular section

--- script/index.js
+++ script/index.js
@@ -4,6 +4,8 @@ if(localStorage.getItem("localAnime")!=null) {
     data = localStorage.getItem("localAnime").split(",");
 
     var j=0;
+console.log(data);
+
     for(i=0;i<data.length;i++) {
         fetch(`https://animeapi.avirana2.repl.co/info?id=${data[i]}`)
         .then((response) => response.json())
@@ -19,7 +21,7 @@ if(localStorage.getItem("localAnime")!=null) {
             var ul = document.getElementById("watchlist");
             var li = document.createElement("li");
             li.setAttribute("style", `order:` + j);
-            li.insertAdjacentHTML('beforeend', `Error fetching anime.`);
+            li.insertAdjacentHTML('beforeend', `<a href="anime.html?id=${data[i]}">${data[i]}</a>`);
         ul.appendChild(li);
           });
     }
@@ -31,7 +33,24 @@ if(localStorage.getItem("localAnime")!=null) {
 
 console.log(localStorage.getItem("localAnime"));
 
-function del(pos) {
-data[pos]="\0";
-localStorage.setItem("localAnime") = data.toString();
-}
+
+
+//popular
+fetch(`https://animeapi.avirana2.repl.co/popular?id=1`)
+        .then((response) => response.json())
+        .then((res) => {
+            for (let j=0;j<15;j++) {
+            var ul = document.getElementById("popularList");
+            var li = document.createElement("li");
+            li.setAttribute("style", `order:` + j);
+            li.insertAdjacentHTML('beforeend', `<a href="anime.html?id=${res[j].id}"><img src="${res[j].img}"><section><span>${res[j].title}</span></section></a>`);
+        ul.appendChild(li);
+            }
+        }).catch((error) => {
+            console.log(error);
+            var ul = document.getElementById("popularList");
+            var li = document.createElement("li");
+            li.setAttribute("style", `order:` + j);
+            li.insertAdjacentHTML('beforeend', `<a href="anime.html?id=${data[i]}">${data[i]}</a>`);
+        ul.appendChild(li);
+          });
